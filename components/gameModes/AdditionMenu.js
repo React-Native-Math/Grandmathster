1ab4c705b84b96869fa0539f647e1c7e1b25f638
B-Abbas <babbas0505@gmail.com>
2022-12-06 22:57:53 +0000

    alignment changes

--- components/gameModes/AdditionMenu.js
+++ components/gameModes/AdditionMenu.js
@@ -1,165 +1,175 @@
-import React, {useState, useEffect} from 'react'
-import {View, StyleSheet, TextInput, Text, Button, Alert} from 'react-native'
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Text,
+  Button,
+  Alert,
+  Pressable,
+} from "react-native";
 import BouncyCheckboxGroup, {
-    ICheckboxButton,
-  } from "react-native-bouncy-checkbox-group";
-import AdditionProblems from './AdditionProblems'
+  ICheckboxButton,
+} from "react-native-bouncy-checkbox-group";
+import AdditionProblems from "./AdditionProblems";
 
+export default function AdditonMunu() {
+  const [difficultyFirstNum, setDifficultyFirstNum] = useState(0);
+  const [difficultySecondNum, setDifficultySecondNum] = useState(0);
+  const [toggle, setToggle] = useState(false);
+  const [questionAmount, setQuestionAmount] = useState(10);
 
-export default function AdditonMunu (){
-    const [difficultyFirstNum, setDifficultyFirstNum] = useState(0)
-    const [difficultySecondNum, setDifficultySecondNum] = useState(0)
-    const [toggle, setToggle] = useState(false)
-    const [questionAmount, setQuestionAmount] = useState(10)
+  const checkboxOptions = [10, 20, 30, "Unlimited"];
 
-    const checkboxOptions = [10, 20, 30, 'Unlimited']
+  const ICheckboxButton = checkboxOptions.map((option, idx) => {
+    return {
+      id: idx,
+      text: option,
+      value: option !== "Unlimited" ? Number(option) : Infinity,
+      fillColor: "#ff7473",
+      unfillColor: "#fbbfbb",
+      textStyle: {
+        textDecorationLine: "none",
+      },
+      style: {
+        marginTop: 16,
+      },
+    };
+  });
 
-    const ICheckboxButton = checkboxOptions.map((option, idx) => {
-      return {
-        id: idx,
-        text: option,
-        value: option !== "Unlimited" ? Number(option) : Infinity,
-        fillColor: "#ff7473",
-        unfillColor: "#fbbfbb",
-        textStyle: {
-          textDecorationLine: "none",
-        },
-        style: {
-          marginTop: 16,
-        },
-      };
-    });
+  // const ICheckboxButton= [
+  //     {
+  //         id:0,
+  //         text:10,
+  //         value:10,
+  //         fillColor: "#ff7473",
+  //         unfillColor: "#fbbfbb",
+  //         textStyle:{
+  //             textDecorationLine:"none",
+  //         },
+  //         style: {
+  //             marginTop: 16,
+  //         }
+  //     },
+  //     {
+  //         id:1,
+  //         text:20,
+  //         value:20,
+  //         fillColor: "#ff7473",
+  //         unfillColor: "#fbbfbb",
+  //         textStyle:{
+  //             textDecorationLine:"none",
+  //         },
+  //         style: {
+  //             marginTop: 16,
+  //         }
+  //     },
+  //     {
+  //         id:2,
+  //         text:30,
+  //         value:30,
+  //         fillColor: "#ff7473",
+  //         unfillColor: "#fbbfbb",
+  //         textStyle:{
+  //             textDecorationLine:"none",
+  //         },
+  //         style: {
+  //             marginTop: 16,
+  //         }
+  //     },
+  //     {
+  //         id:3,
+  //         text:'Unlimited',
+  //         value: Infinity,
+  //         fillColor: "#ff7473",
+  //         unfillColor: "#fbbfbb",
+  //         textStyle:{
+  //             textDecorationLine:"none",
+  //         },
+  //         style: {
+  //             marginTop: 16,
+  //         }
+  //     },
+  // ]
 
-    // const ICheckboxButton= [
-    //     {
-    //         id:0,
-    //         text:10,
-    //         value:10,
-    //         fillColor: "#ff7473",
-    //         unfillColor: "#fbbfbb",
-    //         textStyle:{
-    //             textDecorationLine:"none",
-    //         },
-    //         style: {
-    //             marginTop: 16,
-    //         }
-    //     },
-    //     {
-    //         id:1,
-    //         text:20,
-    //         value:20,
-    //         fillColor: "#ff7473",
-    //         unfillColor: "#fbbfbb",
-    //         textStyle:{
-    //             textDecorationLine:"none",
-    //         },
-    //         style: {
-    //             marginTop: 16,
-    //         }
-    //     },
-    //     {
-    //         id:2,
-    //         text:30,
-    //         value:30,
-    //         fillColor: "#ff7473",
-    //         unfillColor: "#fbbfbb",
-    //         textStyle:{
-    //             textDecorationLine:"none",
-    //         },
-    //         style: {
-    //             marginTop: 16,
-    //         }
-    //     },
-    //     {
-    //         id:3,
-    //         text:'Unlimited',
-    //         value: Infinity,
-    //         fillColor: "#ff7473",
-    //         unfillColor: "#fbbfbb",
-    //         textStyle:{
-    //             textDecorationLine:"none",
-    //         },
-    //         style: {
-    //             marginTop: 16,
-    //         }
-    //     },
-    // ]
+  const handleDifficulty = (e, first, second) => {
+    setDifficultyFirstNum(first);
+    setDifficultySecondNum(second);
+    setToggle(!false);
+  };
 
-    const handleDifficulty = (e,first, second) =>{
-        setDifficultyFirstNum(first)
-        setDifficultySecondNum(second)
-        setToggle(!false)
-    }
+  const handleQuestionAmount = (e, maxQuestions) => {};
 
-    const handleQuestionAmount = (e,maxQuestions) =>{
-
-    }
-
-    return(
+  return (
+    <View>
+      {toggle ? (
+        <AdditionProblems
+          firstNum={difficultyFirstNum}
+          secondNum={difficultySecondNum}
+          maxQuestionsNumber={questionAmount}
+        />
+      ) : (
         <View>
-            {toggle ? 
-                <AdditionProblems 
-                firstNum = {difficultyFirstNum} 
-                secondNum = {difficultySecondNum}
-                maxQuestionsNumber = {questionAmount}/> 
-            :
-            <View>
-                <Text>Number of Questions</Text>
-                <BouncyCheckboxGroup
-                data={ICheckboxButton} 
-                initial={0}
-                style={styles.checkbox}
-                onChange={(selectedItem)=>{
-                    setQuestionAmount(selectedItem.value)
-                }}
-                />
-                <Button
-                style={styles.menuButton}
-                    onPress={(e)=>handleDifficulty(e,10,10)}
-                    title = 'Easy'
-                >
-                </Button>
-                
-                <Button
-                    onPress={(e)=>handleDifficulty(e,100,100)}
-                    title = 'Medium'
-                >
-                    <Text>
-                        Medium
-                    </Text>
-                </Button>
-                <Button
-                    onPress={(e)=>handleDifficulty(e,1000,1000)}
-                    title = 'Hard'
-                >
-                    <Text>
-                        Hard
-                    </Text>
-                </Button>
+          <Text>Number of Questions</Text>
+          <BouncyCheckboxGroup
+            data={ICheckboxButton}
+            initial={0}
+            style={styles.checkbox}
+            onChange={(selectedItem) => {
+              setQuestionAmount(selectedItem.value);
+            }}
+          />
+            <View style={styles.menuContainer}>
+          <Pressable
+            style={styles.menuButton}
+            onPress={(e) => handleDifficulty(e, 10, 10)}
+          >
+            <Text style={styles.menuText}>Easy</Text>
+          </Pressable>
             </View>
-        }
+
+          <Button onPress={(e) => handleDifficulty(e, 100, 100)} title="Medium">
+            <Text>Medium</Text>
+          </Button>
+          <Button onPress={(e) => handleDifficulty(e, 1000, 1000)} title="Hard">
+            <Text>Hard</Text>
+          </Button>
         </View>
-    )
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      alignItems: 'center',
-    },
-    menuButton: {
-      borderRadius: 100,
-      backgroundColor: 'black',
-      padding: 5,
-      width: 150,
-      alignItems: 'center',
-    },
-    menuText: {
-      color: 'white',
-    },
-    separator: {
-      marginVertical: 12,
-    },
-    checkbox: {
-        flexDirection: "column",
-    }
-  });
+  container: {
+    alignItems: "center",
+  },
+  menuContainer: {
+    width: '100vw',
+    display: 'flex',
+    justifyContent: "center",
+    alignItems: "center",
+    paddingBottom: 5,
+  },
+  menuButton: {
+    borderRadius: 50,
+    padding: 5,
+    width: 150,
+    height: 60,
+    color: "white",
+    backgroundColor: "black",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  menuText: {
+    color: "white",
+    fontSize: 18,
+  },
+  separator: {
+    marginVertical: 12,
+  },
+  checkbox: {
+    flexDirection: "column",
+  },
+});
