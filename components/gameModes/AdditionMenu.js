288a428a1cc935d58e8dba36ff0c04b74ce18e66
Bilal Abbas <babbas0505@gmail.com>
2022-12-07 02:09:32 +0000

    tidied up with pressable map

--- components/gameModes/AdditionMenu.js
+++ components/gameModes/AdditionMenu.js
@@ -1,17 +1,9 @@
-import React, { useState, useEffect } from "react";
-import {
-  View,
-  StyleSheet,
-  TextInput,
-  Text,
-  Button,
-  Alert,
-  Pressable,
-} from "react-native";
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text, Pressable } from 'react-native';
 import BouncyCheckboxGroup, {
   ICheckboxButton,
-} from "react-native-bouncy-checkbox-group";
-import AdditionProblems from "./AdditionProblems";
+} from 'react-native-bouncy-checkbox-group';
+import AdditionProblems from './AdditionProblems';
 
 export default function AdditonMunu() {
   const [difficultyFirstNum, setDifficultyFirstNum] = useState(0);
@@ -19,79 +11,24 @@ export default function AdditonMunu() {
   const [toggle, setToggle] = useState(false);
   const [questionAmount, setQuestionAmount] = useState(10);
 
-  const checkboxOptions = [10, 20, 30, "Unlimited"];
-  const difficulties = ['Easy', 'Medium', 'Hard']
+  const checkboxOptions = [10, 20, 30, 'Unlimited'];
+  const difficulties = ['Easy', 'Medium', 'Hard'];
 
   const ICheckboxButton = checkboxOptions.map((option, idx) => {
     return {
       id: idx,
       text: option,
-      value: option !== "Unlimited" ? Number(option) : Infinity,
-      fillColor: "#ff7473",
-      unfillColor: "#fbbfbb",
+      value: option !== 'Unlimited' ? Number(option) : Infinity,
+      fillColor: '#ff7473',
+      unfillColor: '#fbbfbb',
       textStyle: {
-        textDecorationLine: "none",
+        textDecorationLine: 'none',
       },
       style: {
         marginTop: 10,
       },
     };
   });
-
-  // const ICheckboxButton= [
-  //     {
-  //         id:0,
-  //         text:10,
-  //         value:10,
-  //         fillColor: "#ff7473",
-  //         unfillColor: "#fbbfbb",
-  //         textStyle:{
-  //             textDecorationLine:"none",
-  //         },
-  //         style: {
-  //             marginTop: 16,
-  //         }
-  //     },
-  //     {
-  //         id:1,
-  //         text:20,
-  //         value:20,
-  //         fillColor: "#ff7473",
-  //         unfillColor: "#fbbfbb",
-  //         textStyle:{
-  //             textDecorationLine:"none",
-  //         },
-  //         style: {
-  //             marginTop: 16,
-  //         }
-  //     },
-  //     {
-  //         id:2,
-  //         text:30,
-  //         value:30,
-  //         fillColor: "#ff7473",
-  //         unfillColor: "#fbbfbb",
-  //         textStyle:{
-  //             textDecorationLine:"none",
-  //         },
-  //         style: {
-  //             marginTop: 16,
-  //         }
-  //     },
-  //     {
-  //         id:3,
-  //         text:'Unlimited',
-  //         value: Infinity,
-  //         fillColor: "#ff7473",
-  //         unfillColor: "#fbbfbb",
-  //         textStyle:{
-  //             textDecorationLine:"none",
-  //         },
-  //         style: {
-  //             marginTop: 16,
-  //         }
-  //     },
-  // ]
 
   const handleDifficulty = (e, first, second) => {
     setDifficultyFirstNum(first);
@@ -111,31 +48,31 @@ export default function AdditonMunu() {
         />
       ) : (
         <>
-        <View style={styles.questionAmountContainer}>
-          <Text>Number of Questions</Text>
-          <BouncyCheckboxGroup
-            data={ICheckboxButton}
-            initial={0}
-            style={styles.checkbox}
-            onChange={(selectedItem) => {
-              setQuestionAmount(selectedItem.value);
-            }}
-          />
+          <View style={styles.questionAmountContainer}>
+            <Text>Number of Questions</Text>
+            <BouncyCheckboxGroup
+              data={ICheckboxButton}
+              initial={0}
+              style={styles.checkbox}
+              onChange={(selectedItem) => {
+                setQuestionAmount(selectedItem.value);
+              }}
+            />
           </View>
           <View style={styles.buttonsContainer}>
-          <Pressable
-            style={styles.menuButton}
-            onPress={(e) => handleDifficulty(e, 10, 10)}
-          >
-            <Text style={styles.menuText}>Easy</Text>
-          </Pressable>
-          <Button onPress={(e) => handleDifficulty(e, 100, 100)} title="Medium">
-            <Text>Medium</Text>
-          </Button>
-          <Button onPress={(e) => handleDifficulty(e, 1000, 1000)} title="Hard">
-            <Text>Hard</Text>
-          </Button>
-        </View>
+            {difficulties.map((difficulty, idx) => {
+              const maxNum = 10 ** (idx + 1); // sets the maximum possible number for the selected difficulty
+              return (
+                <Pressable
+                  key={idx}
+                  style={styles.menuButton}
+                  onPress={(e) => handleDifficulty(e, maxNum, maxNum)}
+                >
+                  <Text style={styles.menuText}>{difficulty}</Text>
+                </Pressable>
+              );
+            })}
+          </View>
         </>
       )}
     </View>
@@ -144,29 +81,28 @@ export default function AdditonMunu() {
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
+    alignItems: 'center',
   },
   menuContainer: {
-    width: '100vw',
+    // width: '100vw',
     display: 'flex',
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     paddingBottom: 5,
   },
   questionAmountContainer: {
     display: 'flex',
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
     marginTop: 10,
-    backgroundColor: 'red',
   },
   buttonsContainer: {
     display: 'flex',
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: 'green',
-    height: 190,
-    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 220,
+    marginTop: 20,
   },
   menuButton: {
     borderRadius: 50,
@@ -174,20 +110,20 @@ const styles = StyleSheet.create({
     margin: 5,
     width: 150,
     height: 60,
-    color: "white",
-    backgroundColor: "black",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    color: 'white',
+    backgroundColor: 'black',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   menuText: {
-    color: "white",
+    color: 'white',
     fontSize: 18,
   },
   separator: {
     marginVertical: 12,
   },
   checkbox: {
-    flexDirection: "column",
+    flexDirection: 'column',
   },
 });
