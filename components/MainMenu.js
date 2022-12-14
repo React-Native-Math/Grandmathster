641e7b8105826b19a227a1bf9cc88c1a7b6c6951
B-Abbas <babbas0505@gmail.com>
2022-12-14 23:31:35 +0000

    adding bg

--- components/MainMenu.js
+++ components/MainMenu.js
@@ -5,12 +5,14 @@ import {
   Image,
   StyleSheet,
   Pressable,
+  ImageBackground,
 } from "react-native";
 import appLogo from '../assets/img/appLogo.png'
 import Footer from "./Footer";
 import Numpad from "./Numpad";
 
 const Separator = () => <View style={styles.separator} />;
+const bg = { uri: "https://i.pinimg.com/736x/b8/38/af/b838afc8dd3a316f75b93ca9f78ce024.jpg" };
 
 const menuItems = [
   "Addition",
@@ -23,6 +25,7 @@ const menuItems = [
 const MainMenu = ({ navigation }) => {
   const [about, setAbout] = useState(false)
   return (
+      <ImageBackground source={bg} resizeMode="cover" style={styles.bg} >
     <View style={styles.container}>
   {about ? <View style={styles.overlayContainer}>
   <View style={styles.overlay}/>
@@ -58,6 +61,7 @@ const MainMenu = ({ navigation }) => {
       {/* <Numpad /> */}
       </View>
     </View>
+      </ImageBackground>
   );
 };
 
@@ -108,6 +112,10 @@ const styles = StyleSheet.create({
     height: '97%',
     backgroundColor: 'red',
     opacity: 1,
+  },
+  bg: {
+    flex: 1,
+    justifyContent: "center",
   }
 });
 
