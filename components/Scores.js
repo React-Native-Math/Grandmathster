7c6c3626ef22155c477a40a56b1fcd41955c12f9
Bill Munkacsy <billmunkacsy@gmail.com>
2023-01-06 13:32:11 -0500

    got persistance scores saved as an object and am able to display them

--- components/Scores.js
+++ components/Scores.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   View,
   StyleSheet,
@@ -10,17 +10,61 @@ import {
   ImageBackground,
 } from "react-native";
 import { useGlobalState } from "../store/store";
+import OperationScores from "./OperationScores";
 
 
 const Scores = () => {
   const [store, setStore] = useGlobalState('count')
+  const [displayOperation, setDisplayOperation] = useState('')
+  const displayOptions = ['Addition','Subtraction,','Multiplication','Division','Mixed Operations']
+
   return (
     <View>
-      <Text>
-      Scores: {store}
-      </Text>
+    {displayOperation ? 
+      <OperationScores
+        operation={displayOperation}
+      />
+     :
+    <View>
+      {displayOptions.map((option, idx)=>{
+        return(
+          <Pressable
+          key ={idx}
+          style = {styles.menuButton}
+          onPress={(e)=>
+            setDisplayOperation(option)
+          }>
+            <Text style={styles.menuText}>
+              {option}
+            </Text>
+          </Pressable>
+        )
+      })}
+    </View>}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  menuButton: {
+    borderRadius: 50,
+    padding: 5,
+    margin: 5,
+    width: 70,
+    height: 50,
+    backgroundColor: "black",
+    borderWidth: 5,
+    borderColor: "#006b3d",
+    // backgroundColor: '#006b3d',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  menuText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 14,
+  },
+})
 
 export default Scores
--- components/gameModes/GameOver.js
+++ components/gameModes/GameOver.js
@@ -2,23 +2,98 @@ import React, {useEffect, useState} from 'react'
 import {View, StyleSheet, Text, Pressable, ImageBackground, Dimensions} from 'react-native'
 import schoolBackground from '../../assets/img/schoolBackground.png'
 const screen = Dimensions.get('screen')
-import { useGlobalState } from '../../store/store'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-export default function GameOver ({navigation, score, questionAmount, difficulty}) {
+export default function GameOver ({navigation, score, questionAmount, difficulty, operation, timeAtt, timeAmt, custom}) {
     const [message,setMessage]= useState('')
-    const [store, setStore] = useGlobalState('count')
+    const [previousHighScores, setPreviousHighScores] = useState({})
+    const [hit, setHit] = useState(true)
+    const [scoreSaved, setScoreSaved] = useState(false)
+    //AsyncStorage of High Scores notes:
+    //have problems page pass in gameOver screen operation, if timeAtt, and timeAmt
+    //Store objects with keys that look like operation_timeAmt so addition_10 or addition_30
+    //Do not store custom timeAtt amounts.
+    //object will store only 3 scores ie addition_10 = {highScore : 10, midScore : 7, lowScore : 6}
+    //Check to see if timeAtt mode and not custom settings. If timeAtt mode pull scores.
+    //Correctly store the new score in the object or don't store at all if not a new high score.
+    //Write that object to the database overwriting previous object.
+
+    const getHighScores = async () =>{
+        try{
+            const savedHighScores = await AsyncStorage.getItem(operation + '_' + timeAmt)
+            savedHighScores != null ? JSON.parse(savedHighScores) : null
+            console.log(savedHighScores)
+            if(!savedHighScores){
+                setPreviousHighScores({
+                    highScore:0,
+                    midScore:0,
+                    lowScore:0
+                })
+            }
+            else {setPreviousHighScores(savedHighScores)}
+        }catch(e){ 
+            console.log(e)
+        }
+    }
+
+    const setHighScores = async (newHighScoreObj) =>{
+        try{
+            const jsonValue = JSON.stringify(newHighScoreObj)
+            await AsyncStorage.setItem(operation + '_' + timeAmt, jsonValue)
+        }catch(e){
+            console.log(e)
+        }
+    }
+    
+    const writeNewScores = async (previousHighScores, score) => {
+        if(typeof(previousHighScores)!=='object'){
+        previousHighScores = JSON.parse(previousHighScores)
+        }
+      
+        if(score>previousHighScores.highScore){
+            const newHighScores = {
+                lowScore:previousHighScores.midScore,
+                midScore:previousHighScores.highScore,
+                highScore:score,
+            }
+          
+            await setHighScores(newHighScores)
+        }
+        else if(score>previousHighScores.midScore){
+            const newHighScores ={
+                highScore:previousHighScores.highScore,
+                lowScore:previousHighScores.midScore,
+                midScore:score,
+            }
+            await setHighScores(newHighScores)
+        }
+        else if(score>previousHighScores.lowScore){
+            const newHighScores = {
+                ...previousHighScores,
+                lowScore:score
+            }
+            await setHighScores(newHighScores)
+        }
+        setScoreSaved(true)
+    }
+
     useEffect(()=>{
     const accuracy = Math.floor(score/questionAmount*100)
-    if(accuracy===100) { setMessage(`Congratulations you are the Grand Mathster on ${difficulty} mode!`)
-    setStore(store + 1) }
+    if(timeAtt && !custom){
+        getHighScores()
+        
+    }
+    if(accuracy===100) setMessage(`Congratulations you are the Grand Mathster on ${difficulty} mode!`)
     else if(accuracy>90) setMessage(`You are a Mathster on ${difficulty} mode! Keep practicing to become the Grand Mathster!`)
     else if(accuracy>75) setMessage(`You are a Novice on ${difficulty} mode! Keep practicing to become a Mathster!`)
     else if(accuracy>50) setMessage('Keep Practicing!')
     else if(accuracy>0) setMessage('Please review your math facts')
     else if(accuracy===0)setMessage('Stop Guessing')
-    },[])
+    },[hit])
+    
 return(
+
     <ImageBackground source={schoolBackground} style={styles.background}>
         <View style={styles.outerContainer}>
             <Text style={styles.text}>Statistics</Text>  
@@ -33,6 +108,13 @@ return(
         >
         <Text style={styles.buttonText}>Main Menu</Text>    
         </Pressable>
+        {timeAtt && !custom && !scoreSaved ?
+        <Pressable
+        style ={styles.homeButton}
+        onPress={()=>writeNewScores(previousHighScores,score)}
+        >
+        <Text style={styles.buttonText}>Save to High Scores</Text>
+        </Pressable> :null}
         </View>
     </ImageBackground>
     )
